1c3dc569adb33aa7959821525ce2054ccdb4d7df
RJFreebird <97978657+RJFreebird@users.noreply.github.com>
2022-03-05 19:44:25 -0600

    Working Late

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1
+
+console.log(33);
+
+// 2
+/* Strings Boolean Numbers Null Undefined */
+
+// 3
+// Addition is +
+// Subtraction is -
+// Mutltiplication is *
+// Division is /
+// Modulus is %
+// Exponentiation is **
+
+// 4
+console.log(5 % 4);
+console.log(5 % 2);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("HAWK" + "EYE");
+
+// 7
+console.log("Ralph".length + "Grimes".length); 
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
